f82733fe80481534d78043311f6bff9afe27452e
Andres Lopez <andreslo2612@gmail.com>
2026-03-25 22:35:45 +1000

    Fix import statements for LoginPage and RegisterPage components

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
-import LoginPage from './components/LoginPage/LoginPage';
-import RegisterPage from './components/RegisterPage/RegisterPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import { LoginPage } from './components/LoginPage/LoginPage';
+import { RegisterPage } from './components/RegisterPage/RegisterPage';
 function App() {
   const navigate = useNavigate();
   return (
